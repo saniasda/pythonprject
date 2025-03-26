ae20af9dcf7482898d10820a6fafd423bf9a628c
saniasda <saniasdacs@gmail.com>
2025-03-26 18:01:18 +0200

    #Homework 7.1 ; 7.2 ; 7.3 ; 7.4

--- main.py
+++ main.py
@@ -1,75 +1,43 @@
-# Домашнее задание 5.3
-import string
-hashteg = ['Python Community', 'i like python community!', 'Should, I. subscribe? Yes!']
-max_len = 140
-for current_string in hashteg:
-    if len(current_string) > max_len:
-        print(current_string + " length more than " + str(max_len) + " characters")
-        continue
-    current_string = current_string.title()
-    current_string = current_string.replace(" ", "")
-    for symbol in string.punctuation:
-        current_string = current_string.replace(symbol, "")
-    current_string = "#" + current_string
-    print(current_string)
+#Домашнее задание 7.1
+def say_hi(name, age):
+    return f"Hi. My name is {name} and I'm {age} years old"
 
-# Домашнее задание 5.1 =============================================
-import keyword
-import string
+assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", 'Test1'
+print(say_hi("Alex", 32))
+assert say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old", 'Test2'
+print(say_hi("Frank", 68))
+print("Ok")
 
-name = input("Введите имя переменной: ")
-if name in keyword.kwlist:
-    print(False)
-elif name[0].isdigit():
-    print(False)
-elif any(char.isupper() for char in name) or any(char in string.punctuation for char in name):
-    print(False)
-elif name.count('_') > 1:
-    print(False)
-else:
-    print(True)
+#Домашнее задание 7.2
+def correct_sentence(text):
+    text = text[0].upper() + text [1:]
+    if not text.endswith("."):
+        text += "."
+    return text
 
-#Домашнее задание 6.1
-import string
-ALL_LETTERS = string.ascii_letters
-SEPARATOR = "-"
-user_input = input("Введите буквы в формате (а-с): ").strip()
-if len(user_input) == 3:
-    first_letter = user_input[0]
-    second_letter = user_input[2]
-    separator = user_input[1]
-    if first_letter.isalpha() and second_letter.isalpha() and separator == SEPARATOR:
-        start_index = ALL_LETTERS.index(first_letter)
-        end_index = ALL_LETTERS.index(second_letter)
-        if start_index > end_index:
-            start_index, end_index = end_index, start_index
-        result = ALL_LETTERS[start_index:end_index + 1]
-        print(result)
+assert correct_sentence("greetings, friends") == "Greetings, friends.", 'Test1'
+assert correct_sentence("hello") == "Hello.", 'Test2'
+assert correct_sentence("Greetings. Friends") == "Greetings. Friends.", 'Test3'
+assert correct_sentence("Greetings, friends.") == "Greetings, friends.", 'Test4'
+assert correct_sentence("greetings, friends.") == "Greetings, friends.", 'Test5'
+print('ОК')
 
+# Домашнее задание 7.3
+def second_index(text, some_str):
+    first_index = text.find(some_str)
+    if first_index == -1:
+        return None
+    second_index = text.find(some_str, first_index + 1)
+    return second_index if second_index != -1 else None
+assert second_index("sims", "s") == 3, 'Test1'
+assert second_index("find the river", "e") == 12, 'Test2'
+assert second_index("hi", "h") is None, 'Test3'
+assert second_index("Hello, hello", "lo") == 10, 'Test4'
+print('ОК')
 
-# #Домашнее задание 6.2
-seconds = int(input("Введите количество секунд: "))
-if 0 <= seconds < 8640000:
-    days, remaining_seconds = divmod(seconds, 86400)
-    hours, remaining_seconds = divmod(remaining_seconds, 3600)
-    minutes, seconds = divmod(remaining_seconds, 60)
-    if days == 1:
-        day_word = "день"
-    elif 2 <= days <= 4:
-        day_word = "дни"
-    else:
-        day_word = "дней"
-    print(f"{days} {day_word}, {hours:02}:{minutes:02}:{seconds:02}")
-else:
-    print("Число должно быть от 0 до 8639999 секунд")
-
-# #Домашнее задание 6.3
-number = int(input("Введите число: "))
-while number > 9:
-    temp_number = str(number)
-    result = 1
-    for char in temp_number:
-        if char.isdigit():
-            result *= int(char)
-    number = result
-print(number)
+# Домашнее задание 7.4
+def common_elements():
+    lists_of_3 = set(range(0, 100, 3))
+    lists_of_5 = set(range(0, 100, 5))
+    return lists_of_3.intersection(lists_of_5)
+assert common_elements() == {0, 75, 45, 15, 90, 60, 30}
