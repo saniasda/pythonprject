2c3d091754564ba9ab161694b231b448f8e52d9d
saniasda <saniasdacs@gmail.com>
2025-03-31 23:33:18 +0300

    #Homework 8.1 8.2 8.3

--- main.py
+++ main.py
@@ -1,43 +1,41 @@
-#Домашнее задание 7.1
-def say_hi(name, age):
-    return f"Hi. My name is {name} and I'm {age} years old"
+# #Домашнее задание 8.1
+def add_one(some_list):
+    number_str = ""
+    for digit in some_list:
+        number_str += str(digit)
+    number = int(number_str) + 1
+    result = []
+    for digit in str(number):
+        result.append(int(digit))
+    return result
 
-assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", 'Test1'
-print(say_hi("Alex", 32))
-assert say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old", 'Test2'
-print(say_hi("Frank", 68))
-print("Ok")
 
-#Домашнее задание 7.2
-def correct_sentence(text):
-    text = text[0].upper() + text [1:]
-    if not text.endswith("."):
-        text += "."
-    return text
+assert add_one([1, 2, 3, 4]) == [1, 2, 3, 5], 'Test1'
+assert add_one([9, 9, 9]) == [1, 0, 0, 0], 'Test2'
+assert add_one([0]) == [1], 'Test3'
+assert add_one([9]) == [1, 0], 'Test4'
+print("ОК")
 
-assert correct_sentence("greetings, friends") == "Greetings, friends.", 'Test1'
-assert correct_sentence("hello") == "Hello.", 'Test2'
-assert correct_sentence("Greetings. Friends") == "Greetings. Friends.", 'Test3'
-assert correct_sentence("Greetings, friends.") == "Greetings, friends.", 'Test4'
-assert correct_sentence("greetings, friends.") == "Greetings, friends.", 'Test5'
-print('ОК')
+#Домашнее задание 8.2
+def is_palindrome(text):
+    cleaned_text = ''.join(c.lower() for c in text if c.isalnum())
+    return cleaned_text == cleaned_text[::-1]
 
-# Домашнее задание 7.3
-def second_index(text, some_str):
-    first_index = text.find(some_str)
-    if first_index == -1:
-        return None
-    second_index = text.find(some_str, first_index + 1)
-    return second_index if second_index != -1 else None
-assert second_index("sims", "s") == 3, 'Test1'
-assert second_index("find the river", "e") == 12, 'Test2'
-assert second_index("hi", "h") is None, 'Test3'
-assert second_index("Hello, hello", "lo") == 10, 'Test4'
-print('ОК')
 
-# Домашнее задание 7.4
-def common_elements():
-    lists_of_3 = set(range(0, 100, 3))
-    lists_of_5 = set(range(0, 100, 5))
-    return lists_of_3.intersection(lists_of_5)
-assert common_elements() == {0, 75, 45, 15, 90, 60, 30}
+assert is_palindrome('A man, a plan, a canal: Panama') == True, 'Test1'
+assert is_palindrome('0P') == False, 'Test2'
+assert is_palindrome('a.') == True, 'Test3'
+assert is_palindrome('aurora') == False, 'Test4'
+print("ОК")
+
+#Домашнее задание 8.3
+def find_unique_value(some_list):
+    for num in some_list:
+        if some_list.count(num) == 1:
+            return num
+
+
+assert find_unique_value([1, 2, 1, 1]) == 2, 'Test1'
+assert find_unique_value([2, 3, 3, 3, 5, 5]) == 2, 'Test2'
+assert find_unique_value([5, 5, 5, 2, 2, 0.5]) == 0.5, 'Test3'
+print("ОК")
