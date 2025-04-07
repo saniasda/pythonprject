3a564f13f80acd2e04dda9b6c20181c254e34782
saniasda <saniasdacs@gmail.com>
2025-04-07 12:36:49 +0300

    #Homework 9.1 9.2

--- main.py
+++ main.py
@@ -1,41 +1,41 @@
-# #Домашнее задание 8.1
-def add_one(some_list):
-    number_str = ""
-    for digit in some_list:
-        number_str += str(digit)
-    number = int(number_str) + 1
-    result = []
-    for digit in str(number):
-        result.append(int(digit))
+#Домашнее задание 9.1
+def popular_words(text, words):
+    text = text.lower()
+    text_words = text.split()
+    result = {}
+    for word in words:
+        count = 0
+        for w in text_words:
+            if w == word:
+                count += 1
+        result[word] = count
     return result
 
-
-assert add_one([1, 2, 3, 4]) == [1, 2, 3, 5], 'Test1'
-assert add_one([9, 9, 9]) == [1, 0, 0, 0], 'Test2'
-assert add_one([0]) == [1], 'Test3'
-assert add_one([9]) == [1, 0], 'Test4'
-print("ОК")
-
-#Домашнее задание 8.2
-def is_palindrome(text):
-    cleaned_text = ''.join(c.lower() for c in text if c.isalnum())
-    return cleaned_text == cleaned_text[::-1]
+assert popular_words(
+    '''When I was One I had just begun When I was Two I was nearly new''',
+    ['i', 'was', 'three', 'near']
+) == {
+    'i': 4,
+    'was': 3,
+    'three': 0,
+    'near': 0
+}, 'Test1'
 
 
-assert is_palindrome('A man, a plan, a canal: Panama') == True, 'Test1'
-assert is_palindrome('0P') == False, 'Test2'
-assert is_palindrome('a.') == True, 'Test3'
-assert is_palindrome('aurora') == False, 'Test4'
-print("ОК")
+print('OK')
 
-#Домашнее задание 8.3
-def find_unique_value(some_list):
-    for num in some_list:
-        if some_list.count(num) == 1:
-            return num
+#Домашнее задание 9.2
+def difference(*args):
+    if not args:
+        return 0
+    min_val = min(args)
+    max_val = max(args)
+    return round(max_val - min_val, 2)
+
+assert difference(1, 2, 3) == 2, 'Test1'
+assert difference(5, -5) == 10, 'Test2'
+assert difference(10.2, -2.2, 0, 1.1, 0.5) == 12.4, 'Test3'
+assert difference() == 0, 'Test4'
 
 
-assert find_unique_value([1, 2, 1, 1]) == 2, 'Test1'
-assert find_unique_value([2, 3, 3, 3, 5, 5]) == 2, 'Test2'
-assert find_unique_value([5, 5, 5, 2, 2, 0.5]) == 0.5, 'Test3'
-print("ОК")
+print('OK')
