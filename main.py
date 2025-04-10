9801d056440a711e525ea7da46633d1b7dc172a0
saniasda <saniasdacs@gmail.com>
2025-04-10 18:38:29 +0300

    #Homework 10.1 10.2 10.3

--- main.py
+++ main.py
@@ -1,41 +1,43 @@
-#Домашнее задание 9.1
-def popular_words(text, words):
-    text = text.lower()
-    text_words = text.split()
-    result = {}
-    for word in words:
-        count = 0
-        for w in text_words:
-            if w == word:
-                count += 1
-        result[word] = count
-    return result
+#Homework 10.1
+def pow(x):
+    return x ** 2
 
-assert popular_words(
-    '''When I was One I had just begun When I was Two I was nearly new''',
-    ['i', 'was', 'three', 'near']
-) == {
-    'i': 4,
-    'was': 3,
-    'three': 0,
-    'near': 0
-}, 'Test1'
+def some_gen(begin, end, func):
+    yield begin
+    current = begin
+    for _ in range(end - 1):
+        current = func(current)
+        yield current
+
+from inspect import isgenerator
+
+gen = some_gen(2, 4, pow)
+assert isgenerator(gen) == True, 'Test1'
+assert list(gen) == [2, 4, 16, 256], 'Test2'
 
 
 print('OK')
 
-#Домашнее задание 9.2
-def difference(*args):
-    if not args:
-        return 0
-    min_val = min(args)
-    max_val = max(args)
-    return round(max_val - min_val, 2)
+#Домшанее задание 10.2
+def first_word(text):
+    text = text.replace('.', ' ').replace(',', ' ')
+    return text.split()[0]
 
-assert difference(1, 2, 3) == 2, 'Test1'
-assert difference(5, -5) == 10, 'Test2'
-assert difference(10.2, -2.2, 0, 1.1, 0.5) == 12.4, 'Test3'
-assert difference() == 0, 'Test4'
+assert first_word("Hello world") == "Hello", 'Test1'
+assert first_word("greetings, friends") == "greetings", 'Test2'
+assert first_word("don't touch it") == "don't", 'Test3'
+assert first_word(".., and so on ...") == "and", 'Test4'
+assert first_word("hi") == "hi", 'Test5'
+assert first_word("Hello.World") == "Hello", 'Test6'
 
 
+print('OK')
+
+#Домашнее задание 10.3
+def is_even(digit):
+    return digit % 2 == 0
+
+assert is_even(2) == True, 'Test1'
+assert is_even(5) == False, 'Test2'
+assert is_even(0) == True, 'Test3'
 print('OK')
